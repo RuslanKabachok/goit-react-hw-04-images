a56730de92ca5ba97e94f3ba61e179aa354b4d9c
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-08-26 16:51:03 +0300

    upd

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,16 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { ModalWindow, BackDrop } from './Modal.styled';
 
 export const Modal = ({ largeImg, closeModal }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', closeModal);
+
+    return () => {
+      window.removeEventListener('keydown', closeModal);
+    };
+  });
+
   return (
     <BackDrop onClick={e => closeModal(e)}>
       <ModalWindow>
